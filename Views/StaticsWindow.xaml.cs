2aa99699b7185d841037421461359ad0ff98ed6d
luisc <luiscarlos031100@gmail.com>
2026-06-02 21:28:00 -0600

    - Added "System Idle Time"

--- Views/StaticsWindow.xaml.cs
+++ Views/StaticsWindow.xaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Windows;
+using System.Windows.Threading;
 using PausaVital.Services;
 
 namespace PausaVital.Views
@@ -7,6 +9,7 @@ namespace PausaVital.Views
     {
         private readonly ApiService apiService;
         private readonly int userId;
+        private readonly DispatcherTimer liveTimer; // NEW: Timer for live stats
 
         public StatisticsWindow(int userId)
         {
@@ -15,10 +18,19 @@ namespace PausaVital.Views
             this.apiService = new ApiService();
 
             Loaded += OnWindowLoaded;
+            Closed += OnWindowClosed; // Clean up when closed
+
+            // NEW: Setup the live timer
+            liveTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            liveTimer.Tick += OnLiveTimerTicked;
         }
 
         private async void OnWindowLoaded(object sender, RoutedEventArgs e)
         {
+            // 1. Fetch static data from SQLite
             var stats = await apiService.GetUserStatsAsync(userId);
 
             if (stats.HasValue)
@@ -29,11 +41,27 @@ namespace PausaVital.Views
                 CompletedText.Text = stats.Value.completed.ToString();
                 FailedText.Text = stats.Value.failed.ToString();
             }
+
+            // 2. Start live monitoring
+            liveTimer.Start();
+        }
+
+        // NEW: Update the idle text every second
+        private void OnLiveTimerTicked(object? sender, EventArgs e)
+        {
+            TimeSpan idle = ActivityMonitor.GetIdleTime();
+            LiveIdleText.Text = $"{(int)idle.TotalMinutes:D2}:{idle.Seconds:D2}";
         }
 
         private void OnCloseButtonClicked(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        // NEW: Stop the timer to free up memory
+        private void OnWindowClosed(object? sender, EventArgs e)
+        {
+            liveTimer.Stop();
         }
     }
 }
